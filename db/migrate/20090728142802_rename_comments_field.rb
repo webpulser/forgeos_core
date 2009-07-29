d0b1e4fa81930103bfe4ed33bdc11a2e902631cc
rendelin <rendelin@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-07-29 07:48:13 +0000

    bug fix: migration to rename comment table had a syntax error

--- db/migrate/20090728142802_rename_comments_field.rb
+++ db/migrate/20090728142802_rename_comments_field.rb
@@ -1,12 +1,12 @@
 class RenameCommentsField < ActiveRecord::Migration
   def self.up
-    rename_column :comments :user_id, :person_id
+    rename_column :comments, :user_id, :person_id
     remove_index :comments, ["user_id"]
     add_index :comments, ["person_id"], :name => "fk_comments_person"
   end
 
   def self.down
-    rename_column :comments :person_id, :user_id
+    rename_column :comments, :person_id, :user_id
     remove_index :comments, ["person_id"]
     add_index :comments, ["user_id"], :name => "fk_comments_user"
   end
