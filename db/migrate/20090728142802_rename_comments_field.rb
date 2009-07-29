1cb76a59f45e90a1817f751383b4dc9f56b48f20
rendelin <rendelin@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-07-29 08:09:32 +0000

    bug fix: malformed migration for comment

--- db/migrate/20090728142802_rename_comments_field.rb
+++ db/migrate/20090728142802_rename_comments_field.rb
@@ -1,13 +1,13 @@
 class RenameCommentsField < ActiveRecord::Migration
   def self.up
+    remove_index :comments, :name => "fk_comments_user"
     rename_column :comments, :user_id, :person_id
-    remove_index :comments, ["user_id"]
     add_index :comments, ["person_id"], :name => "fk_comments_person"
   end
 
   def self.down
+    remove_index :comments, :name => "fk_comments_person"
     rename_column :comments, :person_id, :user_id
-    remove_index :comments, ["person_id"]
     add_index :comments, ["user_id"], :name => "fk_comments_user"
   end
 end
