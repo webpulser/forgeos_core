28792dad3f1da6bc04515ce4f768d9753d17bc8c
clepagnot <clepagnot@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-08-03 15:42:19 +0000

    - add url_generator

--- lib/forgeos.rb
+++ lib/forgeos.rb
@@ -11,4 +11,22 @@ module Forgeos
   ]
   # Set site's menu
   Menu = []
+
+  def self.url_generator(phrase = '')
+    url = phrase.dup
+    { %w(á à â ä ã Ã Ä Â À) => 'a',
+      %w(é è ê ë Ë É È Ê €) => 'e',
+      %w(í ì î ï I Î Ì) => 'i',
+      %w(ó ò ô ö õ Õ Ö Ô Ò) => 'o',
+      %w(œ) => 'oe',
+      %w(ß) => 'ss',
+      %w(ú ù û ü U Û Ù) => 'u',
+      %w(\/ \| \\ \\& = #) => '',
+      %w(\s+) => '_'
+    }.each do |ac,rep|
+      url.gsub!(Regexp.new(ac.join('|')), rep)
+    end
+
+    url.underscore.gsub(/(^_+|_+$)/,'')
+  end
 end
