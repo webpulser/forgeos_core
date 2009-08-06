eceb15b20eb2e3f2208b8c4ef5af79dfcc8bf621
cveneziani <cveneziani@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-08-06 09:00:10 +0000

    refactored dataTables tags helper methods

--- app/helpers/admin/base_helper.rb
+++ app/helpers/admin/base_helper.rb
@@ -22,33 +22,11 @@ module Admin::BaseHelper
     end
   end
 
-  def dataTables_tag(columns_count=1, action_column=true)
+  def dataTables_tag(columns_count=1, action_column=true, sorting=false)
     columns_count -= 1 if action_column
     columns = columns_count.times.collect{'null'}
     columns << "{ 'bSearchable': false, 'bSortable': false }" if action_column
-
-    javascript_tag "
-    $(function(){
-      oTable = $('#table').dataTable({ 
-        'aoColumns': [ #{columns.join(',')} ],
-        'oLanguage': { 
-          'sProcessing' : '#{I18n.t('jquery.dataTables.oLanguage.sProcessing')}',
-          'sLengthMenu':'#{I18n.t('jquery.dataTables.oLanguage.sLengthMenu')}',
-          'sZeroRecords':'#{I18n.t('jquery.dataTables.oLanguage.sZeroRecords')}',
-          'sInfo':'#{I18n.t('jquery.dataTables.oLanguage.sInfo')}',
-          'sInfoEmpty':'#{I18n.t('jquery.dataTables.oLanguage.sInfoEmpty')}',
-          'sSearch':'#{I18n.t('jquery.dataTables.oLanguage.sSearch')}'
-        }
-      });
-    });"
-  end
-
-  def dataTables_medias_tag(columns_count=5, action_column=true)
-    columns_count -= 1 if action_column
-    columns = columns_count.times.collect{'null'}
-
-    columns << "{ 'bSearchable': false, 'bSortable': false }" if action_column
-    columns[0] = "{ 'bVisible': false, 'sType': 'numeric' }"
+    columns[0] = "{ 'bVisible': false, 'sType': 'numeric' }" if sorting
 
     javascript_tag "
     $(function(){
