6e369a2287918b3b2d020c8d128724f4388e57e5
fcogez <fcogez@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-08-26 13:18:13 +0000

    Updating upload

--- lib/flash_session_cookie_middleware.rb
+++ lib/flash_session_cookie_middleware.rb
@@ -6,13 +6,25 @@ class FlashSessionCookieMiddleware
     @session_key = session_key
   end
 
+#  def call(env)
+#    if env['HTTP_USER_AGENT'] =~ /^(Adobe|Shockwave) Flash/
+#      params = env["rack.request.form_hash"]
+#      unless params[@session_key].nil?
+#        env['HTTP_COOKIE'] = [ @session_key, params[@session_key].gsub(' ','%2B') ].join('=').freeze
+#      end
+#    end
+#    @app.call(env)
+#  end
+
   def call(env)
-    if env['HTTP_USER_AGENT'] =~ /^(Adobe|Shockwave) Flash/
-      params = env["rack.request.form_hash"]
-      unless params[@session_key].nil?
-        env['HTTP_COOKIE'] = [ @session_key, params[@session_key].gsub(' ','%2B') ].join('=').freeze 
+      if env['HTTP_USER_AGENT'] =~ /^(Adobe|Shockwave) Flash/
+        params = Rack::Request.new(env)
+
+        unless params[@session_key].nil?
+          env['HTTP_COOKIE'] = [ @session_key, params[@session_key].gsub(' ','%2B') ].join('=').freeze
+        end
       end
-    end
-    @app.call(env)
+      @app.call(env)
   end
+
 end
