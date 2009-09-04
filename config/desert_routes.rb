9aeb74782143f426e3d8294008ff751999fe4236
fcogez <fcogez@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-09-04 15:24:20 +0000

    Updating locales, routes, & fixtures for medias

--- config/desert_routes.rb
+++ config/desert_routes.rb
@@ -3,14 +3,16 @@ namespace :admin do |admin|
   admin.login '/login', :controller => 'sessions', :action => 'new'
   admin.notifications '/notifications', :controller => 'base', :action => 'notifications'
 
+#  admin.connect 'medias/:file_type', :controller => :medias, :action => :index
+#  admin.connect 'medias/:file_type/new', :controller => :medias, :action => :new
+  
   admin.resource :session
   admin.resources :account
   admin.resources :admins
   admin.resources :roles, :member => { :activate => :post }
   admin.resources :rights
-  admin.resources :medias, :except => [:edit], :member => { :download => :get, :sort => :post }
+  admin.resources :medias, :except => [:edit], :member => { :download => :get, :sort => :post }, :path_prefix => 'admin/:file_type'
   admin.resources :attachments, :controller => :medias, :except => [:edit], :member => { :download => :get, :sort => :post }
-
 #  admin.root :controller => 'account'
   admin.root :controller => 'products'
 end
