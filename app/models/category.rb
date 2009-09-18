a96af753bd93cc7abf4d8fbf4e50cce65e7ba44a
cveneziani <cveneziani@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-09-18 09:39:16 +0000

    - init-tree for all index categories jstree

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,8 @@
 class Category < ActiveRecord::Base
   acts_as_tree
   
-  has_and_belongs_to_many :attachments, :list => true, :order => 'position'
+  # TODO
+  #has_and_belongs_to_many :attachments, :list => true, :order => 'position'
   
   validates_presence_of :name
 
