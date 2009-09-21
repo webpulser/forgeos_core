19885d21a1d21b8090f4056584325b1cce96f15a
clepagnot <clepagnot@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-09-21 14:33:54 +0000

    - set date of statistic_counter default to current

--- app/models/statistic_counter.rb
+++ app/models/statistic_counter.rb
@@ -3,14 +3,21 @@ class StatisticCounter < ActiveRecord::Base
   belongs_to :element, :polymorphic => true, :dependent => :destroy
 
   validates_uniqueness_of :element_id, :scope => [ :date, :element_type ]
+  def initialize
+    super
+    set_default_date_to_current
+  end
 
   def increment_counter
-     sc = StatisticCounter.find_by_element_id_and_date_and_element_type( element_id, date, element_type )
+     sc = self.class.find_by_element_id_and_date_and_element_type( element_id, date, element_type )
      if sc
        sc.increment!(:counter)
      else
        false
      end
   end
-
+private
+  def set_default_date_to_current
+    self.date = Date.current
+  end
 end
