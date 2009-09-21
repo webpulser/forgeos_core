1a75df47628767cad9170033cebf738b8c49228a
clepagnot <clepagnot@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-09-21 14:04:35 +0000

    - prepare date selection in statistics

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -1,4 +1,10 @@
 class Admin::StatisticsController < Admin::BaseController
+  before_filter :get_date, :only => :index
   def index
+  end
+
+private
+  def get_date
+    @date = Date.current
   end
 end
