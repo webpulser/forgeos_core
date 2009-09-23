78478de0c4410dabdfd1e09623b765dcc6052255
clepagnot <clepagnot@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-09-23 09:30:41 +0000

    - import date locales from rails-i18n project

--- config/initializers/time_formats.rb
+++
@@ -1,7 +0,0 @@
-# Date and Time formats
-Time::DATE_FORMATS[:slashed_fr] = "%d/%m/%Y"
-Date::DATE_FORMATS[:slashed_fr] = "%d/%m/%Y"
-
-Date::DATE_FORMATS[:long_ordinal] = lambda { |date| date.strftime("%B #{date.day.ordinalize}") }
-Date::DATE_FORMATS[:long_ordinal_with_day] = lambda { |date| date.strftime("%A, %B #{date.day.ordinalize}") }
-
--- config/locales/fr.rb
+++ config/locales/fr.rb
@@ -1,17 +1,9 @@
 {
   :fr => {
-    :date => {
-      :formats => {
-        :long_ordinal => lambda { |date| "%B #{date.day.ordinalize}, %Y" }
-      }
-    },
     :time => {
-      :formats => {
-        :long_ordinal => lambda { |time| "%B #{time.day.ordinalize}, %Y %H:%M" }
-      },
       :time_with_zone => {
         :formats => {
-          :default => lambda { |time| "%Y-%m-%d %H:%M:%S #{time.formatted_offset(false, 'UTC')}" }
+          :default => lambda { |time| "%d/%m/%Y %H:%M:%S #{time.formatted_offset(false, 'UTC')}" }
         }
       }
     }
