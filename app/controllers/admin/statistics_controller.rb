d9512d1dd1bb5b298b58c79e29aabeeb7f857ac8
clepagnot <clepagnot@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-09-25 15:30:25 +0000

    - set default timestamp on stats to last 7 days

--- app/controllers/admin/statistics_controller.rb
+++ app/controllers/admin/statistics_controller.rb
@@ -9,13 +9,12 @@ private
       case params[:timestamp]
       when 'month'
         Date.current.ago(1.month).to_date..Date.current
-      when 'week'
-        Date.current.ago(1.week).to_date..Date.current
       when 'yesterday'
         Date.yesterday..Date.current
       when 'today'
         Date.current..Date.current
       else # week
+        params[:timestamp] = 'week'
         Date.current.ago(1.week).to_date..Date.current
       end
   end
