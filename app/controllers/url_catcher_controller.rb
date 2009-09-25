52cf9c0b3d005815f3af8be4f0daef3269ce82df
clepagnot <clepagnot@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-09-25 13:49:00 +0000

    - oops

--- app/controllers/url_catcher_controller.rb
+++ app/controllers/url_catcher_controller.rb
@@ -2,7 +2,6 @@ class UrlCatcherController < ApplicationController
   after_filter :visitor_count
 
   def visitor_count
-    logger.debug('count'+'***'*1000)
     unless cookies[:visitor_counter]
       VisitorCounter.new.increment_counter
       cookies[:visitor_counter] = true
