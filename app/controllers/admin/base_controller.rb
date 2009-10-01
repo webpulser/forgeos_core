7281b23cd4c3a4f2c6caf9f3572aaa56ad1bf423
clepagnot <clepagnot@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-10-01 08:32:52 +0000

    - skip login for admin url generation

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -1,7 +1,7 @@
 class Admin::BaseController < ApplicationController
   include AuthenticatedSystem
   layout 'admin'
-  before_filter :login_required
+  before_filter :login_required, :except => :url
 
   def notifications
     @notifications = {}
