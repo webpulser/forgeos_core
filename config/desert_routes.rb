2830c2983c0bfcf846d54e427e85dc2d569cdb5d
cveneziani <cveneziani@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-10-02 13:31:59 +0000

    routes - reordered routes and added categories

--- config/desert_routes.rb
+++ config/desert_routes.rb
@@ -4,23 +4,24 @@ namespace :admin do |admin|
   admin.notifications '/notifications', :controller => 'base', :action => 'notifications'
   admin.statistics '/statistics', :controller => 'statistics'
   admin.statistics_graph '/statistics/graph', :controller => 'statistics', :action => 'graph'
-
-#  admin.connect 'medias/:file_type', :controller => :medias, :action => :index
-#  admin.connect 'medias/:file_type/new', :controller => :medias, :action => :new
-  
+ 
   admin.resource :session
   admin.resources :account
   admin.resources :admins
   admin.resources :roles, :member => { :activate => :post }
   admin.resources :rights
+
+  admin.resources :categories, :member => { :add_element => :post }
+  %w(picture media pdf doc video attachment admin role right).each do |category|
+    admin.resources "#{category}_categories", :controller => 'categories', :requirements => { :type => "#{category}_category" }
+  end
+
+  admin.library '/library', :controller => 'attachments', :file_type => 'picture'
   %w(medias pictures docs pdfs videos attachments role right).each do |resources_alias|
     route_options = { :controller => :attachments, :member => { :download => :get }, :except => [:new] }
     route_options[:path_prefix] = 'admin/:file_type' if %w(attachments).include?(resources_alias)
     admin.resources resources_alias.to_sym, route_options
   end
-  %w(picture media pdf doc video attachment admin role right).each do |category|
-    admin.resources "#{category}_categories", :controller => 'categories', :requirements => { :type => "#{category}_category" }
-  end
-  admin.library '/library', :controller => 'attachments', :file_type => 'picture'
+
   admin.root :controller => 'dashboard'
 end
