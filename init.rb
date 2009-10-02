4051b13a59f3d87b9f81a718c20225896db8a2d2
fcogez <fcogez@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-10-02 13:33:03 +0000

    updating forgeos init

--- init.rb
+++ init.rb
@@ -1,4 +1,4 @@
-I18n.load_path += Dir[Rails.root.join('vendor', 'plugins', 'forgeos_core', 'config', 'locales', '*.{rb,yml}')]
+I18n.load_path += Dir[File.join(RAILS_ROOT, 'vendor', 'plugins', 'forgeos_core', 'config', 'locales', '**', '*.{rb,yml}')]
 require 'forgeos'
 # Specify gems to use
 config.gem 'mime-types', :lib => 'mime/types'
