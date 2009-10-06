f92d68863f4d3b371f2466a8710cb7ddfbe285c0
jclefrancois <jclefrancois@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-10-06 15:50:07 +0000

    update design

--- public/javascripts/admin/inits/forgeos_core/header.js
+++ public/javascripts/admin/inits/forgeos_core/header.js
@@ -2,7 +2,4 @@ jQuery(document).ready(function(){
   //add spans in current's page tab
   $('#menu .current').append('<span class="after-current"></span>');
   $('#menu .current').prepend('<span class="before-current"></span>');
-
-  //remove borders arround submenu current item
-  $('#submenu .current').next('li').addClass('next');
 });
