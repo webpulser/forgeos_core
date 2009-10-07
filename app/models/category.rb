2d1e3413e0650ce041afff9f811e54ecdf1e826a
clepagnot <clepagnot@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-10-07 08:34:11 +0000

    - update attachments associations

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,10 +1,7 @@
 class Category < ActiveRecord::Base
   acts_as_tree
   
-  # TODO
-  has_and_belongs_to_many :attachments, :list => true, :order => 'position'
-  has_and_belongs_to_many :attachments2, :list => true, :order => 'position', :join_table => 'attachments_elements', :foreign_key => 'element_id', :association_foreign_key => 'attachment_id', :class_name => 'Attachment'
-  
+  has_and_belongs_to_many :attachments, :list => true, :order => 'position', :join_table => 'attachments_elements', :foreign_key => 'element_id'
   validates_presence_of :name
 
   # Returns the level of <i>Category</i>
