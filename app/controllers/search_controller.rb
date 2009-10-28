7088095d980aacd7b17540f1ee348495c1a70c3f
clepagnot <clepagnot@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-10-28 13:42:49 +0000

    * fix search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,9 +1,14 @@
 class SearchController < ApplicationController
+  before_filter :init_index, :only => [:index]
   def index
     keywords = params[:keywords] || session[:keywords]
     session[:keywords] = keywords
     search_keyword = SearchKeyword.find_by_keyword(keywords) || SearchKeyword.create(:keyword => keywords)
     search_keyword.search_keyword_counters.new.increment_counter
+  end
+
+  private
+  def init_index
     @items = []
   end
 end
