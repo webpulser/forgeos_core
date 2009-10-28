d33f872a59db17134f567a0528db6c04628d9ca7
clepagnot <clepagnot@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-10-28 14:48:39 +0000

    * fix search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,14 +1,14 @@
 class SearchController < ApplicationController
-  before_filter :init_index, :only => [:index]
+  before_filter :init_index, :only => :index
   def index
-    keywords = params[:keywords] || session[:keywords]
-    session[:keywords] = keywords
-    search_keyword = SearchKeyword.find_by_keyword(keywords) || SearchKeyword.create(:keyword => keywords)
+    search_keyword = SearchKeyword.find_by_keyword(@keywords) || SearchKeyword.create(:keyword => @keywords)
     search_keyword.search_keyword_counters.new.increment_counter
   end
 
-  private
+private
   def init_index
     @items = []
+    @keywords = params[:keywords]
+    render if @keywords.blank?
   end
 end
