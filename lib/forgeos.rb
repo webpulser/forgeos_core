33c4d78cda43b01dd747971961380ebcf4f41ed0
clepagnot <clepagnot@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-10-30 09:39:32 +0000

    * ADMIN fix error fields display

--- lib/forgeos.rb
+++ lib/forgeos.rb
@@ -6,8 +6,8 @@ require 'forgeos/statistics'
 
 ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   error_message = instance.object.errors.on(instance.method_name)
-  if error_message
-    "#{html_tag}<div class='field_error'>#{error_message.is_a?(Array) ? error_message.first : error_message}</div>"
+  if error_message && !(html_tag =~ /^<label|type="hidden"/)
+    "#{html_tag}<div class='field error'><span class='small-icons message'>#{error_message.is_a?(Array) ? error_message.first : error_message}</span></div>"
   else
     html_tag
   end
