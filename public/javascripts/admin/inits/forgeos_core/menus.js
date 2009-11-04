e4df8aba6261d866fd67f006f324032f54ffcc77
cveneziani <cveneziani@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-11-04 15:05:44 +0000

    navigation - add root menu link

--- public/javascripts/admin/inits/forgeos_core/menus.js
+++ public/javascripts/admin/inits/forgeos_core/menus.js
@@ -1,4 +1,111 @@
 jQuery(document).ready(function(){
+
+  // Add root menu link
+  $('.button-menu-link').live('click',function(){
+    var menu = $('#menu-tree').children('ul');
+    var new_menu_list = '<li class="file last closed">';
+    new_menu_list += $('#empty_menu_link').html().replace(/EMPTY_ID/g, false_id);
+    new_menu_list += '</li>';
+
+    $(menu).append(new_menu_list);
+    false_id--;
+    return false;
+  });
+
+  // Add menu link
+  $('.tree-menu-tree li .menu_link .action-links .add-green-plus').live('click',function(){
+    // reference
+    var menu_list = $(this).parents('li:first');
+    var menuContainer = $(this).parents('.menu_link');
+
+    console.info('hu');
+    console.info($(this));
+
+    // new link
+    var new_menu_list = $('<li class="file last closed">').append(jquery_obj_to_str(menuContainer));
+    var new_menuContainer = $(new_menu_list).find('.menu_link');
+   
+    var menuLinks = $(new_menuContainer).children('.tree-link');
+    var edition_block = $(menuLinks).filter('.editing');
+    var link = $(menuLinks).filter('a');
+    
+    $(menu_list).parent().append(new_menu_list);
+
+    // set attributes
+    $(edition_block).find('input, textarea, select').each(function(){
+      var id = $(this).attr('id');
+      var name = $(this).attr('name');
+
+      var attributes = name.split('[');
+      var attribute = attributes[attributes.length-1].replace(']','');
+
+      // replace rails_id in id and name html attributes
+      if (id != ""){      
+        var previous_id = attributes[attributes.length-2].replace(']','');
+        var re = new RegExp('^(.*)'+previous_id+'(.*?)$');
+
+        $(this).attr('id', id.replace(re, '$1'+false_id+'$2'));
+        $(this).attr('name', name.replace(re, '$1'+false_id+'$2'));
+      }
+
+      // reset attributes values
+      switch(attribute)
+        {
+        // FIXME: link_to, interactivity
+        case 'title':
+          $(this).val('');
+          link.find('.name').html('');
+          break;
+
+        case 'url':
+          $(this).val('');
+          link.attr('href', '');
+          break;
+
+        case 'active':
+          status_span = link.find('.status');
+          // update select
+          $(this).val(1);
+          rebuild_custom_select('.select-status');
+
+          // set visible
+          if (status_span.hasClass('see-off')){
+            status_span.removeClass('see-off');
+            status_span.addClass('see-on');
+          }
+          break;
+
+        case 'id':
+          $(this).remove();
+          break;
+         
+        case 'kind':
+          $(this).val('ExternalLink');
+          break;
+
+        case 'delete', 'target_id', 'target_type', 'position':
+          $(this).val('');
+          break;
+        }
+    });
+
+    // set classes of spans to external
+    var linked_to_span = $(edition_block).find('.linked-to-span');
+    $(linked_to_span).removeClass();
+    $(linked_to_span).addClass('small-icons external linked-to-span');
+    $(linked_to_span).html('');
+
+    var name_span = $(menuLinks).find('.name');
+    $(name_span).removeClass();
+    $(name_span).addClass('small-icons external name');
+    $(name_span).html('');
+
+    // set edition block visible
+    menuLinks.toggle();
+    false_id--;
+    return false;
+  });
+
   // Edition mode in menu link edit
   $('.tree-menu-tree li .menu_link .action-links .edit-link, .tree-menu-tree li .menu_link .actions .back-link').live('click',function(){
     var menuContainer = $(this).parents('.menu_link');
@@ -53,7 +160,7 @@ jQuery(document).ready(function(){
 
   // Remove menu link
   $('.tree-menu-tree li .menu_link .action-links .destroy-link').live('click',function(){
-    var menu_list = $(this).parents('li:first'); 
+    var menu_list = $(this).parents('li:first');
 
     // set the menu link and all its children to deleted
     $(menu_list).find('.menu_link').each(function(){
