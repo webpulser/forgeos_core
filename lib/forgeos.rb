4436712d163833392336ef0ee8eaf14b9f53867f
clepagnot <clepagnot@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-11-12 11:15:17 +0000

    * fix has_many_polymorphs load

--- lib/forgeos.rb
+++ lib/forgeos.rb
@@ -13,8 +13,14 @@ ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
   end
 end
 
-require 'has_many_polymorphs'
-require 'has_many_polymorphs/extensions'
-class ActiveRecord::Base
-  extend ActiveRecord::Associations::PolymorphicClassMethods 
-end 
+begin
+  require 'has_many_polymorphs'
+  has_many_polymorphs_loaded = true
+rescue LoadError
+  has_many_polymorphs_loaded = false
+end
+
+if has_many_polymorphs_loaded
+  require 'has_many_polymorphs/extensions'
+  ActiveRecord::Base.send(:extend,ActiveRecord::Associations::PolymorphicClassMethods)
+end
