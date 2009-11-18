a1d64786041d68508093afa049161ba506cb3a95
clepagnot <clepagnot@6fb54821-cf04-4776-85c6-6d318fcd397b>
2009-11-18 09:42:48 +0000

    * oops

--- public/javascripts/admin/inits/forgeos_core/rights.js
+++ public/javascripts/admin/inits/forgeos_core/rights.js
@@ -1,20 +1,21 @@
 jQuery(document).ready(function(){
   /* Inline create right*/
-  $('.create-right').bind('click', function(){
+  $('.create-right').live('click', function(){
 
     /* Create new row-right */
-    new_row = '<tr id="new_right" class="even ui-draggable">';
-      new_row += '<td><div class="small-icons right-ico">&nbsp;</div></td>';
-      new_row += '<td><input type="text" name="right[name]" size="15" /></td>';
-      new_row += '<td><input type="text" name="right[controller_name]" size="15" /></td>';
-      new_row += '<td><input type="text" name="right[action_name]" size="15" /></td>';
-      new_row += '<td>';
-         new_row += '<a href="#" onclick="$(\'#form_right\').trigger(\'onsubmit\'); return false;"><span class="small-icons green-add-right-icon">&nbsp;</span></a>';
-         new_row += '<a href="#" onclick="discard(); return false;"><span class="small-icons red-delete-right-icon">&nbsp;</span></a>';
-      new_row += '</td>';
-    new_row += '</tr>';
+    new_row = '<tr id="new_row" class="even ui-draggable">' +
+     '<td><div class="small-icons right-ico">&nbsp;</div></td>' +
+     '<td><input type="text" name="right[name]" size="15" /></td>' +
+     '<td><input type="text" name="right[controller_name]" size="15" /></td>' +
+     '<td><input type="text" name="right[action_name]" size="15" /></td>' +
+     '<td>' +
+     '<a href="#" onclick="$(\'#form_right\').trigger(\'onsubmit\'); return false;"><span class="small-icons green-add-right-icon">&nbsp;</span></a>' +
+     '<a href="#" onclick="discard(); return false;"><span class="small-icons red-delete-right-icon">&nbsp;</span></a>' +
+     '</td>' +
+    '</tr>';
 
     $('#table').prepend(new_row);
+    
     /* Create the form, and move it around the table */
     $('#table').wrap('<form action="/admin/rights/create" id="form_right" method="POST" onsubmit="' + form_ajax_right('/admin/rights/create', 'post') + '"></form>');
 
@@ -46,7 +47,7 @@ jQuery(document).ready(function(){
     $('table').wrap('<form action="/admin/rights/'+right_id+'" id="form_right" method="PUT" onsubmit="'+form_ajax_right('/admin/rights/'+right_id, 'put')+'"></form>');
 
     /* Create a row with values of the selected rights */
-    new_row = '<tr id="new_right" class="odd ui-draggable">';
+    new_row = '<tr id="new_row" class="odd ui-draggable">';
       new_row += '<td><div class="small-icons right-ico">&nbsp;</div></td>';
       new_row += '<td><input type="text" value="'+ cell_name_value +'" name="right[name]" size="15"/></td>';
       new_row += '<td><input type="text" value="'+ cell_controller_value +'" name="right[controller_name]" size="15"/></td>';
@@ -85,7 +86,7 @@ jQuery(document).ready(function(){
     $('#table').wrap('<form action="/admin/rights/create" id="form_right" method="POST" onsubmit="' + form_ajax_right('/admin/rights/create', 'post') + '"></form>');
 
     /* Create new row-right */
-    new_row = '<tr id="new_right" class="'+ row.attr('class') +' ui-draggable">';
+    new_row = '<tr id="new_row" class="'+ row.attr('class') +' ui-draggable">';
       new_row += '<td><div class="small-icons right-ico">&nbsp;</div></td>';
       new_row += '<td><input type="text" value="'+ cell_name_value +'" name="right[name]" size="15" /></td>';
       new_row += '<td><input type="text" value="'+ cell_controller_value +'" name="right[controller_name]" size="15" /></td>';
