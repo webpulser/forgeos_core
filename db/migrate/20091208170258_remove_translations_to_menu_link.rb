42cda073ee6eba1a35176c84c5755a085d900849
cyrill62 <cyril.lepagnot@webpulser.com>
2010-02-03 09:50:29 +0100

    * move menus & menu-links to forgeos_cms

--- db/migrate/20091208170258_remove_translations_to_menu_link.rb
+++
@@ -1,9 +0,0 @@
-class RemoveTranslationsToMenuLink < ActiveRecord::Migration
-  def self.up
-    remove_column :menu_links, :title
-    remove_column :menu_links, :url
-  end
-
-  def self.down
-  end
-end
