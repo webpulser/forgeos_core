185ac749d333405a357105d43fc03181c9a2268e
cyrill62 <cyril.lepagnot@webpulser.com>
2010-03-11 15:15:49 +0100

    add helper to build admin save button

--- app/helpers/admin/base_helper.rb
+++ app/helpers/admin/base_helper.rb
@@ -1,4 +1,10 @@
 module Admin::BaseHelper
+  def Forgeos_save_buttons(back_path=admin_root_path)
+    content_tag(:div, :class => 'interact-links') do
+      fg_submit_tag('save_changes') + t('or') + link_to(t('cancel').capitalize, back_path, :class => 'back-link')
+    end
+  end
+
   def hide_html_elements_onload(element_ids)
     return javascript_tag("$('##{element_ids.join(",#")}').hide()")
   end 
