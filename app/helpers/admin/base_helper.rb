bd36b7d734e69fbf380ac6464cfe6a0e355b0393
cyrill62 <cyril.lepagnot@webpulser.com>
2010-03-17 10:10:51 +0100

    cleanup admin/base_helper

--- app/helpers/admin/base_helper.rb
+++ app/helpers/admin/base_helper.rb
@@ -1,21 +1,4 @@
-module Admin::BaseHelper
-  def hide_html_elements_onload(element_ids)
-    return javascript_tag("$('##{element_ids.join(",#")}').hide()")
-  end 
-
-  def link_to_effect_toggle(title,element_id,effect = :toggle_appear )
-    return link_to_function(title,visual_effect(effect,element_id))
-  end
-
-  def yield_for_tools
-    content_for :tools, link_to(I18n.t('back').capitalize,:back, :class => 'back')
-    out = ''
-    @content_for_tools.each do |content|
-      out += content_tag('li', content) unless content.blank?
-    end
-    return out
-  end
-  
+module Admin::BaseHelper 
   def dataTables_tag(options = {})
     id = options[:id].nil? ? 'table' : options[:id]
     columns = options[:columns]
