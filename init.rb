d557fd27f69e78b1a5b5769838396b3a6da6c56e
cyrill62 <cyril.lepagnot@webpulser.com>
2010-03-23 18:44:24 +0100

    fix settings

--- init.rb
+++ init.rb
@@ -22,8 +22,8 @@ if ActiveRecord::Base.connection.tables.include?(Setting.table_name) && settings
   config.time_zone = settings.time_zone
   I18n.default_locale = settings.lang.to_sym
   ActionMailer::Base.delivery_method = settings.mailer ? settings.mailer.delivery_method : :smtp
-  ActionMailer::Base.smtp_settings = settings.smtp_settings
-  ActionMailer::Base.sendmail_settings = settings.sendmail_settings
+  ActionMailer::Base.smtp_settings = settings.smtp_settings.marshal_dump if settings.smtp_settings
+  ActionMailer::Base.sendmail_settings = settings.sendmail_settings.marshal_dump if settings.sendmail_settings
 end
 
 puts 'Forgeos Core loaded'
