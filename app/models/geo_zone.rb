97b50e5dbe8aa81a4eae2ac15fef7c82799e2f9c
jclefrancois <jclefrancois@MacBook-de-jclefrancois.local>
2010-03-30 14:18:51 +0200

    move tojstree function to geoZone and add a param

--- app/models/geo_zone.rb
+++ app/models/geo_zone.rb
@@ -1,3 +1,13 @@
 class GeoZone < ActiveRecord::Base
   acts_as_tree
+  
+  def to_jstree(element_to_count)
+       hash = {}
+       hash[:attributes] = { :id => "#{self.class.to_s.underscore}_#{id}", :type => 'folder' }
+       hash[:data] = { :title => "#{name}<span>#{self.send(element_to_count).count}</span>", :attributes => { :class => 'big-icons', :style => 'category_picture' }}
+       unless children.empty?
+         hash[:children] = children.collect{ |g| g.to_jstree(element_to_count)}
+       end
+       hash
+  end
 end
