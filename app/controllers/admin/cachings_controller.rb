c405e5b2db02049dd0ef65cc25bdea11186fce07
Jean-Charles Lefrancois <jcl@webpulser.com>
2010-10-12 12:11:50 +0200

    refactoring on cahcing controller

--- app/controllers/admin/cachings_controller.rb
+++ app/controllers/admin/cachings_controller.rb
@@ -4,40 +4,35 @@ class Admin::CachingsController < Admin::BaseController
   def index
     @files = []
     directory = Rails.configuration.action_controller.page_cache_directory
-    get_file(directory)
+    if directory
+      get_file(directory)
+    end
   end
 
   def create
     if params[:commit] == t('caching.delete.all').capitalize
-
       files = params[:hidden_files]
+    else
+      files = params[:file]
+    end
 
+    if files
       files.values.each do |file|
         unless File.delete(file)
           flash[:error] = t('caching.delete.failed').capitalize
           return redirect_to :action => 'index'
         end
       end
-
     else
-      if files = params[:file]
-        files.values.each do |file|
-          unless File.delete(file)
-            flash[:error] = t('caching.delete.failed').capitalize
-            return redirect_to :action => 'index'
-          end
-        end
-      else
-        return redirect_to :action => 'index'
-        flash[:error] = t('caching.delete.failed').capitalize
-      end
+      return redirect_to :action => 'index'
+      flash[:error] = t('caching.no_files').capitalize
     end
 
 
     begin
       FileUtils.remove_dir(Rails.cache.cache_path, true)
     rescue
-      p 'file does not exist'
+      p 'This cache directory does not exist'
     end
 
     flash[:success] = t('caching.delete.create').capitalize
