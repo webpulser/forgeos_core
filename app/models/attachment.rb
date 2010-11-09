101413543b4e81a2bd5bc5785cd1d86899710f67
Sébastien Dumont <sebastien.dumont@webpulser.com>
2010-11-09 17:49:57 +0100

    add scope to fetch attachments which are linked

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,9 @@
 class Attachment < ActiveRecord::Base
   has_and_belongs_to_many   :attachment_categories, :readonly => true, :join_table => 'categories_elements', :foreign_key => 'element_id', :association_foreign_key => 'category_id'
   before_save :fill_blank_name_with_filename 
+  has_many :attachment_links
+
+  named_scope :linked, lambda {{:include => :attachment_links, :conditions => {:attachment_links => {:element_type => 'Product'}}}}
 
   define_index do
     indexes filename, :sortable => true
