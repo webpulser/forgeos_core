8139cf81c99b9a0a4246bb8c34548eb46700e1af
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2011-02-08 12:11:02 +0100

    refs #7547

--- public/javascripts/admin/functions/forgeos_core/inputs.js
+++ public/javascripts/admin/functions/forgeos_core/inputs.js
@@ -8,11 +8,13 @@ function update_block_container_positions(container){
 }
 
 function remove_rails_nested_object(selector){
-  if ($(selector).find('input.delete').size() > 0) {
-    $(selector).find('input.delete').val(1);
+  var line = $(selector);
+  var del = line.find('input.destroy');
+  if (del.length > 0) {
+    del.val(1);
     tmce_unload_children(selector);
-    $(selector).hide('explode', 500);
+    line.hide('explode',{}, 3000);
   } else {
-    $(selector).hide('explode',500).remove();
+    line.hide('explode',{}, 3000,function(){ $(this).remove(); });
   }
 }
