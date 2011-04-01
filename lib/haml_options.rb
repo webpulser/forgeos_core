65e10dc2612b2c27aac3da672b35183271b1577f
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2011-04-01 17:08:41 +0200

    fix haml_options for production

--- lib/haml_options.rb
+++ lib/haml_options.rb
@@ -1,9 +1,10 @@
 # Load Haml and Sass
 require 'haml'
 Haml.init_rails(binding)
-if Rails.env == :production
+if Rails.env == 'production'
   Haml::Template.options[:ugly] = true
   Sass::Plugin.options[:style] = :compressed
+  Sass::Plugin.options[:debug_info] = false
 else
   Sass::Plugin.options[:style] = :compact
   Sass::Plugin.options[:debug_info] = true
