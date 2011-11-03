c12e1002a57323d088bd6e523ffdb2a1eccb397a
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2011-11-04 00:40:03 +0100

    fix ruby 1.9 Mail::Body to String Convertion in tests

--- test/unit/user_notifier_test.rb
+++ test/unit/user_notifier_test.rb
@@ -4,12 +4,12 @@ class UserNotifierTest < ActionMailer::TestCase
   test "should send new password" do
     mail = UserNotifier.reset_password(people(:user), 'new_password')
     assert_equal ['john.doe@forgeos.com'], mail.to
-    assert_match /new_password/, mail.body
+    assert_match /new_password/, mail.body.to_s
   end
 
   test "should send import finished" do
     mail = UserNotifier.import_finished('test@test.org', {:total => 100})
     assert_equal ['test@test.org'], mail.to
-    assert_match /100/, mail.body
+    assert_match /100/, mail.body.to_s
   end
 end
