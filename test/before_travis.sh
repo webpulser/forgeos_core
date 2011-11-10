147a87039119deff4c3781cbd76a51931798bbf5
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2011-11-10 09:05:14 +0100

    fix fixtures preload

--- test/before_travis.sh
+++ test/before_travis.sh
@@ -4,6 +4,7 @@ cp "test/files/database.yml.$DB" "test/dummy/config/database.yml"
 
 # Set up database
 echo "Creating databases for $DB and loading schema"
+cp -r "test/fixtures" "test/dummy/test/"
 cd "test/dummy"
 bundle exec rake db:create
 bundle exec rake db:test:prepare db:fixtures:load
