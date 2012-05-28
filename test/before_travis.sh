92ae8b8699e4fd1fafdc6aaa07c4d7074a1d4417
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2012-05-28 17:02:06 +0200

    fix mysql databse creation for jruby on travis

--- test/before_travis.sh
+++ test/before_travis.sh
@@ -6,6 +6,10 @@ cp "test/files/database.$DB.yml" "test/dummy/config/database.yml"
 echo "Creating databases for $DB and loading schema"
 cp -r "test/fixtures" "test/dummy/test/"
 cd "test/dummy"
-bundle exec rake db:create
+if [ "$DB" = "mysql" ]; then
+  mysql -e 'create database forgeos_core_test;'
+else
+  bundle exec rake db:create
+fi
 echo "Loading Fixtures"
 bundle exec rake db:test:prepare db:fixtures:load
