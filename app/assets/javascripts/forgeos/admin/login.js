2ad70d569dedb7b892a97e5c4aea1757b2784b11
Cyril Lepagnot <cyril.lepagnot@webpulser.com>
2012-06-14 02:13:26 +0200

    use twitter_bootstrap in admin person_session

--- app/assets/javascripts/forgeos/admin/login.js
+++ app/assets/javascripts/forgeos/admin/login.js
@@ -1,2 +1,8 @@
-//= require forgeos/admin/functions/forgeos_core/notifications
-//= require forgeos/admin/inits/forgeos_core/clicks
+/*
+*= require jquery
+*= require jquery_ujs
+*= require bootstrap-all
+*= require forgeos/admin/functions/forgeos_core/notifications
+*= require forgeos/admin/inits/forgeos_core/notifications
+*= require forgeos/admin/inits/forgeos_core/clicks
+*/
